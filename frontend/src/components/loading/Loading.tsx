5bcf69447d1d391d1ecd544992f46d224144efcb
nhan4599 <nhan0385790927@gmail.com>
2024-05-07 19:14:43 +0700

    [Add] Add loading functionality

--- frontend/src/components/loading/Loading.tsx
+++ frontend/src/components/loading/Loading.tsx
@@ -1,17 +1,45 @@
-import "../../index.css"
-import {useLottie} from "lottie-react";
+import "../../index.css";
+import { useLottie } from "lottie-react";
 import LoadingAnimation from "../../lottie/loading.json";
+import { useAppSelector } from "../../features/hooks";
+import { selectLoadingState } from "../../features/slices/loading.slice";
+import { useEffect, useMemo } from "react";
+  
+const options = {
+  animationData: LoadingAnimation,
+  loop: true
+};
 
 const Loading = () => {
-    const options = {
-        animationData: LoadingAnimation,
-        loop: true
-      };
-    
-      const { View } = useLottie(options);
-    return (
-        <div className='loading-container'>{View}</div>
-    );
+  const isOpen = useAppSelector(selectLoadingState)
+
+  const { View, animationItem } = useLottie(options);
+
+  const style = useMemo(() => {
+    if (!isOpen) {
+      return {
+        display: 'none'
+      }
+    }
+
+    return undefined
+  }, [isOpen])
+
+  useEffect(() => {
+    if (isOpen) {
+      animationItem?.play()
+    } else {
+      animationItem?.pause()
+    }
+  }, [isOpen, animationItem])
+
+  useEffect(() => {
+    return () => {
+      animationItem?.destroy()
+    }
+  }, [animationItem])
+  
+  return <div style={style} className="loading-container">{View}</div>;
 };
 
 export default Loading;
