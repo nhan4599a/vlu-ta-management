8187a0b743a871e3a7e00ead2da1038fc1e5a443
buiphuoctai145 <55799927+buiphuoctai145@users.noreply.github.com>
2024-05-08 13:19:57 +0700

    amend Dropzone Component

--- frontend/src/page/Welcome.tsx
+++ frontend/src/page/Welcome.tsx
@@ -1,7 +1,13 @@
+import DropzoneComponent from "../components/dashboard/ImportDropZone";
 
 const Welcome = () => {
+  const handleFileAdded = (file: File) => {
+    console.log("File added:", file);
+  };
+
   return (
     <>
+      <DropzoneComponent onFileAdded={handleFileAdded} />
       <h3 className="mt-2 ms-2">Welcome username!</h3>
     </>
   );
