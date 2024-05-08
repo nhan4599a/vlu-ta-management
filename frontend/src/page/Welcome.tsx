9f9e2a06dae7dad261003c4345ebf946e2bed38b
buiphuoctai145 <55799927+buiphuoctai145@users.noreply.github.com>
2024-05-08 19:44:21 +0700

    amend Dropzone and add xlsx svg icon

--- frontend/src/page/Welcome.tsx
+++ frontend/src/page/Welcome.tsx
@@ -1,13 +1,6 @@
-import DropzoneComponent from "../components/dashboard/ImportDropZone";
-
 const Welcome = () => {
-  const handleFileAdded = (file: File) => {
-    console.log("File added:", file);
-  };
-
   return (
     <>
-      <DropzoneComponent onFileAdded={handleFileAdded} />
       <h3 className="mt-2 ms-2">Welcome username!</h3>
     </>
   );
--- frontend/src/page/classmanagement/ImportSectionClassList.tsx
+++ frontend/src/page/classmanagement/ImportSectionClassList.tsx
@@ -3,11 +3,14 @@ import "../../index.css";
 import ImportButton from "../../components/buttons/ImportButton";
 
 const ImportSectionClassList = () => {
+  const handleFileAdded = (file: File) => {
+    console.log("File added:", file);
+  };
   return (
     <div>
       <h2 className="display-5 mt-2 mb-3">Import danh sách lớp học phần</h2>
       <div className="shadow p-5 rounded-5 bg-white"> 
-        <DropzoneComponent />
+      <DropzoneComponent onFileAdded={handleFileAdded} />
         <ImportButton />
       </div>
     </div>
