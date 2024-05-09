5d83ba18893f72adc9c43944f8903a3658a577f1
nhan4599 <nhan0385790927@gmail.com>
2024-05-09 15:19:18 +0700

    [Add] Add cors to api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express'
+import cors from 'cors'
 import termController from './controllers/term/term.controller'
 import recruitController from './controllers/recruit/recruit.controller'
 import { IBaseRequest } from './types/integration.types'
@@ -9,6 +10,7 @@ import { standardizeResponse } from './middlewares/hooks.middleware'
 
 const app: Express = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use(standardizeResponse)
