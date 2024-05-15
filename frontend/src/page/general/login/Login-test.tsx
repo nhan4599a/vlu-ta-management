c084ff80c3ee86d6f9e0d9698d75a509f4808c46
buiphuoctai <buiphuoctai1405@gmail.com>
2024-05-15 22:31:19 +0700

    duplicate login page to testing

---
+++ frontend/src/page/general/login/Login-test.tsx
@@ -0,0 +1,47 @@
+import { Image, Button } from "react-bootstrap";
+import { useMsal } from "@azure/msal-react";
+import { constant } from "../../../constants";
+import { useAppDispatch } from "../../../features/hooks";
+import { showMessageDialog } from "../../../features/slices/messages.slice";
+import "../../../index.css";
+
+const Login = () => {
+  const { instance } = useMsal();
+  const dispatch = useAppDispatch();
+
+  const login = () => {
+    instance.loginRedirect(constant.authentication.loginRequest).catch(() => {
+      dispatch(showMessageDialog("Failed to login"));
+    });
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-illustrator">
+        <h2 className="position-relative display-1 text-uppercase fw-bolder">
+          Website quản lý trợ giảng
+        </h2>
+        <Image
+          className="position-relative"
+          src="/images/main-illustrator.png"
+          alt="login"
+        />
+      </div>
+      <div className="d-flex flex-column justify-content-center align-items-center login-action">
+        <h2 className="position-relative display-4 fw-bolder">Đăng nhập</h2>
+        <Button className="btn-sm mt-4 mb-5" onClick={login} variant="primary">
+          Admin
+        </Button>
+        <Button className="btn-sm mt-4 mb-5" onClick={login} variant="primary">
+          Giảng viên
+        </Button>
+        <Button className="btn-sm mt-4 mb-5" onClick={login} variant="primary">
+          Sinh viên
+        </Button>
+        <p>Đăng nhập bằng tài khoản Văn Lang để bắt đầu</p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
