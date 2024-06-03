6089b8497bcfe95731830907022a99892656d59d
nhan4599 <nhan0385790927@gmail.com>
2024-06-03 20:39:46 +0700

    [Update] Minimize server build

--- backend/src/controllers/public/public.controller.ts
+++ backend/src/controllers/public/public.controller.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/:fileName", (req, res) => {
   const { params } = createTypedRequest(req);
-
+  res.setHeader('Cache-Control', 'public, max-age=31557600');
   res.sendFile(params.fileName, {
     root: "public",
   });
